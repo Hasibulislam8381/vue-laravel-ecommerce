4d5c7a0bfbccdefa2bfa773978cb7fbc8a1b6e3c
Himel <hasibulislam8381@gmail.com>
2024-01-27 02:05:11 +0600

    COmmit By Himel

---
+++ resources/js/pages/Users/ListUsers.vue
@@ -0,0 +1,73 @@
+<script setup>
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+
+const users = ref([]);
+
+const getUsers = () => {
+    axios.get('/api/users')
+        .then((response) => {
+            users.value = response.data;
+
+        });
+};
+
+onMounted(() => {
+    getUsers();
+});
+</script>
+
+
+<template>
+    <div>
+        <div class="content-header">
+
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Users</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Users</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="content">
+            <div class="container-fluid">
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Register Date</th>
+                            <th scope="col">Role</th>
+                            <th scope="col">Options</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="user in users" :key="user.id">
+                            <th scope="row">{{ user.id }}</th>
+                            <td>{{ user.name }}</td>
+                            <td>mark@gmail.com</td>
+                            <td>12 aug,2023</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+
+
+                    </tbody>
+                </table>
+
+
+
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/js/routes.js
@@ -0,0 +1,33 @@
+import Dashboard from "./components/Dashboard.vue";
+import ListAppointments from "./pages/Appointments/ListAppointments.vue";
+import ListUsers from "./pages/Users/ListUsers.vue";
+import UpdateProfile from "./pages/Profile/UpdateProfile.vue";
+import UpdateSettings from "./pages/Settings/UpdateSettings.vue";
+
+export default [
+    {
+        path: "/admin/dashboard",
+        name: "admin.dashboard",
+        component: Dashboard,
+    },
+    {
+        path: "/admin/appointments",
+        name: "admin.appointments",
+        component: ListAppointments,
+    },
+    {
+        path: "/admin/users",
+        name: "admin.users",
+        component: ListUsers,
+    },
+    {
+        path: "/admin/profile",
+        name: "admin.profile",
+        component: UpdateProfile,
+    },
+    {
+        path: "/admin/settings",
+        name: "admin.settings",
+        component: UpdateSettings,
+    },
+];
