b930f3f584a209346a348be013652a3fcd32bd20
Himel <hasibulislam8381@gmail.com>
2024-01-27 23:33:14 +0600

    COmmit By Himel

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -14,4 +14,12 @@ class UserController extends Controller
        return $users;
 
     }
+    public function store(){
+      return User::create([
+        'name'=>request('name'),
+        'email'=>request('email'),
+        'password'=> bcrypt(request('password')),
+      ]);
+
+    }
 }
--- resources/js/pages/Users/ListUsers.vue
+++ resources/js/pages/Users/ListUsers.vue
@@ -1,8 +1,14 @@
 <script setup>
 import axios from 'axios';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, reactive } from 'vue';
 
 const users = ref([]);
+
+const form = reactive({
+    name: '',
+    email: '',
+    password: ''
+});
 
 const getUsers = () => {
     axios.get('/api/users')
@@ -11,6 +17,21 @@ const getUsers = () => {
 
         });
 };
+
+const createUser = () => {
+
+    axios.post('/api/users', form)
+        .then((response) => {
+            users.value.unshift(response.data);
+            form.name = '',
+                form.email = '',
+                form.password = '',
+
+                $('#createUserModal').modal('hide');
+
+        });
+
+}
 
 onMounted(() => {
     getUsers();
@@ -40,6 +61,10 @@ onMounted(() => {
 
         <div class="content">
             <div class="container-fluid">
+                <div class="add_user_btn">
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#createUserModal">Add New
+                        User</button>
+                </div>
                 <table class="table">
                     <thead class="thead-dark">
                         <tr>
@@ -55,7 +80,7 @@ onMounted(() => {
                         <tr v-for="user in users" :key="user.id">
                             <th scope="row">{{ user.id }}</th>
                             <td>{{ user.name }}</td>
-                            <td>mark@gmail.com</td>
+                            <td>{{ user.email }}</td>
                             <td>12 aug,2023</td>
                             <td></td>
                             <td></td>
@@ -70,4 +95,41 @@ onMounted(() => {
             </div>
         </div>
     </div>
+
+    <!-- Button trigger modal -->
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="createUserModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add a new User</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="">
+                        <label for="name">Name</label>
+                        <input v-model="form.name" type="name" class="form-control" placeholder="Enter Full Name">
+                        <label for="email">Email</label>
+                        <input v-model="form.email" type="email" class="form-control" placeholder="Enter User Email">
+                        <label for="password">Password</label>
+                        <input v-model="form.password" type="password" class="form-control" placeholder="Enter Password">
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button @click="createUser" type="button" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+<style>
+.add_user_btn {
+    padding-bottom: 10px;
+}
+</style>
