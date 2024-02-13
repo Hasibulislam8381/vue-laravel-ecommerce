18211368be91d5ca9409eac10f4a84c9ff018f6a
Himel <hasibulislam8381@gmail.com>
2024-02-13 23:44:01 +0600

    commit by himel

--- app/Http/Controllers/Admin/AppointmentController.php
+++ app/Http/Controllers/Admin/AppointmentController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Appointment;
 use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
 {
     //
     public function index(){
-        dd("here");
+        return Appointment::query()
+                ->with('client:id,first_name,last_name')
+                ->latest()
+                ->paginate()
+                ->through(fn ($appointment)=>[
+                'id'=>$appointment->id,
+                'start_time'=>$appointment->start_time->format('Y-m-d h:i A'),
+                'end_time'=>$appointment->end_time->format('Y-m-d h:i A'),
+                'status'=>[
+                    'name'=>$appointment->status->name,
+                    'color'=>$appointment->status->color(),
+                ],
+                'client'=>$appointment->client,
+                ]);
     }
 }
